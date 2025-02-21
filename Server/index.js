d9968fbcace2b4c822d76cfc8ea36fc52e1cea30
Salih-alnor <salih.gad@icloud.com>
2025-02-21 17:04:20 +0300

    Initial commit

---
+++ Server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("./config/connectionDB");
+const AuthApi = require("./apis/AuthApi");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 5500;
+
+
+app.post("/", async (req, res, next) => {
+  res.json({ message: "server is listening" });
+});
+
+app.use("/api/v1/auth", AuthApi);
+
+
+app.all('*', (req, res, next) => {
+    const error = new Error(`the url ${req.url} is not found`);
+    next(error);
+ })
+ 
+ app.use((err, req, res, next) => {
+     res.status(err.code || 403).json({error: err.message})
+ })
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
