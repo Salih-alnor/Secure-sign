30ccee25efc0c0f6100cd11d6f72bd31cf6d742b
Salih-alnor <salih.gad@icloud.com>
2025-03-02 06:00:48 +0300

    d

--- Server/config/connectionDB.js
+++ Server/config/connectionDB.js
@@ -2,26 +2,8 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 
-// const connection = mysql.createConnection(process.env.BASE_URL_DB);
-// connection.connect((err, connection) => {
-//   if (err) {
-//     console.log(err.message, connection);
-//   } else {
-//     console.log("Connected database! ");
-//   }
-// });
-
 let connection;
 
-
-// const dbConfig = {
-//   host: process.env.HOST ||'localhost',
-//   user: process.env.USER ||'user',
-//   password: process.env.PASSWORD,
-//   database: process.env.DATABASE,
-//   ssl: {"rejectUnauthorized":true}
- 
-// };
 function handleDisconnect() {
   connection = mysql.createConnection(process.env.DATABASE_URL);
 
